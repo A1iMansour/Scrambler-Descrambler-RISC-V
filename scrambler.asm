9c22d23cbd28d0a13cd2cf6eccea1ffb4899744e
A1iMansour <ali.mansour5531@outlook.com>
2023-04-30 23:23:33 +0300

    almost done

--- scrambler.asm
+++ scrambler.asm
@@ -1,21 +1,19 @@
 
 .data
 input: .string "enter the message here: "
-input2: .string "enter scrambeled text: "
-input3: .string "enter seed: "
+input2: .string "\n enter scrambeled text: "
+input3: .string "\n enter seed: "
 output1: .string "Scrambled message: "
-output2: .string "original message; "
-array: .space 256 # allocate space to store list of indices
+output2: .string "\n original message: "
+debug: .string "\n"
+output3:.string "\n this is the seed you will use for descrambler:"
 buffer: .space 256	 # allocate a buffer to store the input string
 buffer2: .space 256 	# allocate another buffer to store input that will consist of scrambeled text
 seed: .space 256 	#allocate place to store seed
 .text
 main:
 
-####################################################
 
-###########SHOULD SEED BE INPUT???????????
-###################################
 
 #######SCRAMBLER#############
 
@@ -39,44 +37,49 @@ l:
   addi x2,x2,1 	#move to next byte
   j l
   exit:
-  
+     
 
 #Scrambler key using random number generator
-li x11, 1947		#load random's seed                     <---------------------------------       ****** SEED  ****** 
+li x11, 563		#load random's seed                     <---------------------------------       ****** SEED  ****** 
 la x12,buffer		#load input message address to x12
-addi x14, x0, 0 	# initialize the loop counter
-la x22,array		#load address of array
-loop:
-   
-    remu x7, x11, x13	# generate a random integer less than length
-    add x20,x22,x14	#address of index to store random index
-    sb x7,0(x22)	#store index
-     
-    add x15, x0, x7	#compute the index of the character to swap
-    rem x15, x15, x13	#to make sure I am chosing charecter at position i less than length of message
-   
-     #swap the current character with the character at the random position
-    add x15,x15,x12	# address of charecter at random position
-    add x17,x14,x12	#address of charecter at current position
-    lb x16, 0(x15)   	# load the character at the random position
-    lb x18, 0(x17)   	# load the current character
-    sb x18, 0(x15)   	# store the character at the current position
-    sb x16, 0(x17)   	# store the current character at the random position
+la x30,buffer		#load input message address to x30
+li x14, 0 	# initialize the loop counter
 
-    
-    addi x14, x14, 1	# increment the loop counter
-    blt  x14, x13, loop	#check if counter is less than length
+loop:
+   	la x12,buffer
+   	remu x7, x11, x13	# generate a random integer less than length
+    	addi x11,x11,1 #change seed to change random number generated
+    	add x12, x12, x7	# address of charecter at random position
+    			
+    	 #swap the current character with the character at the random position	
+
+	lb x16, 0(x12)   	# load the character at the random position
+	lb x18, 0(x30)   	# load the current character
+	sb x18, 0(x12)   	# store the character at the current position
+	sb x16, 0(x30)   	# store the current character at the random position
+	addi x30,x30,1	#address of charecter at current position    
+	addi x14, x14, 1	# increment the loop counter
+	blt  x14, x13, loop	#check if counter is less than length
     
 li a7,4		# set the print syscall number
 la,a0 ,output1	# load the address of the message
 ecall     	# execute syscall to print the message
+
 
 #printing scrambeled text
 li a7,4   	# set the print syscall number
 la a0,buffer 	# load the address of the message
 ecall     	# execute syscall to print the message
     
+li a7,4   	# set the print syscall number
+la a0,output3 	# load the address of the message
+ecall 		# execute syscall to print the message
 
+addi x11,x11,-1 #compute seed for descrambler
+
+li a7,1		# set the print syscall number for integer
+mv a0, x11	# load the address of the seed
+ecall		# execute syscall to print the message
 
 #######Descrambler#############
 li a7,4		# set the print syscall number
@@ -88,43 +91,36 @@ la a0,buffer2  	# load the address of the input buffer
 li a1,256 	# load max number of characters to read
 ecall     	# execute the syscall to read the input string
 
-la x3,buffer2  	#load input message to x3
-
 li a7,4		# set the print syscall number
 la a0,input3	#load the address of the message
 ecall     	# execute syscall to print the message
 
 li a7,5		# load the syscall number for reading an integer 
-la a0,buffer2  	# load the address of the input buffer
+la a0,seed  	# load the address of the input buffer
 li a1,256 	# load max number of characters to read
 ecall     	# execute the syscall to read the input integer
 
-la x4,seed	#load seed to x4
-
 ##This time loop should work backward
+lb x11,seed
+			li a7,1		# set the print syscall number
+			mv a0,x11	#load the address of the message
+			ecall  
 la x5, buffer2       	# load the address of the input buffer
-la x6,buffer2		#load the address of input buffer again to be used to get address of charecter
 add x5,x5,x13		#load address of last charecter
-addi x5,x5,-1		#since x13 is length
-la x24,array		#load address of list of indices
-add x24,x24,x13  		#load address of last index
-addi x24,x24,-1		#since x13 is length
-addi x14, x0, 0 	# initialize the loop counter
-
+li x14, 0 		# initialize the loop counter
+neg x25,x13		#to be used for loop condition
 loop2:
-	sub x24,x24,x14 	#compute index of random index
-   	
-   	  #swap the current character with the character at the random position
-    	add x25,x24,x6		# address of charecter at random position
-   	sub x26,x5,x14		#address of charecter at current position
-   	lb x27, 0(x25)   	# load the character at the random position
-   	lb x28, 0(x26)   	# load the current character
-   	sb x28, 0(x25)   	# store the character at the current position
-    	sb x27, 0(x26)   	# store the current character at the random position
-
-    
-    	addi x14, x14, 1	# increment the loop counter
-	blt  x14, x13, loop2	#check if counter is less than length
+	la x6,buffer2		#toad the address of input buffer again to be used to get address of charecter
+	remu x7, x11, x13	#generate seeds backward
+	addi x11,x11,-1 	#decrement the seed
+	add x6,x7,x6		#compute address of charecte at random index
+	addi x5,x5,-1		#compute address of current charecter
+   	lb x16, 0(x6)   	# load the character at the random position
+   	lb x18, 0(x5)   	# load the current character
+   	sb x18, 0(x6)   	# store the character at the current position
+    	sb x16, 0(x5)   	# store the current character at the random position
+    	addi x14, x14, -1	# increment the loop counter
+	bgt  x14, x25, loop2	#check if counter is less than length
 	
 li a7,4		# set the print syscall number
 la,a0 ,output2	# load the address of the message
